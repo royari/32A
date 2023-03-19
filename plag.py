54b4e97e8dbc0f856a8fee244a3bbd0844799832
AlphaRoy14 <AlphaRoy14@gmail.com>
2023-03-19 14:54:20 -0700

    update plag script to only read .py files

--- plag.py
+++ plag.py
@@ -7,8 +7,7 @@ Before running this script:
 1. export the submissions from gradescope
 2. unzip and directory and rename it to `submissions`
 3. export the grades from gradescope
-3. delete all the columns except first name, last name, submission_id
-4. save the file as 'HW3_ID.csv'
+4. save the file as 'HW3.csv' or the equivalent
 5. run this script
 """
 
@@ -32,8 +31,7 @@ def print_init_message():
     1. Export the submissions from gradescope
     2. Unzip and directory and rename it to `submissions`
     3. Export the grades from gradescope
-    3. Delete all the columns except first name, last name, submission_id
-    4. Save the file as 'HW3_ID.csv'
+    4. Save the file as 'HW3_ID.csv' or the equivalent
     5. Run this script
     ######################################################################
     """
@@ -41,7 +39,7 @@ def print_init_message():
 
 
 
-def create_submission_name_map(metadata_file: str = 'HW3_ID.csv') -> dict:
+def create_submission_name_map(metadata_file: str = 'HW3.csv') -> dict:
     """
     Creates a map of submission_id and student name from submission metadata file
     """
@@ -53,10 +51,13 @@ def create_submission_name_map(metadata_file: str = 'HW3_ID.csv') -> dict:
         for line in infile:
             line = line.strip()
             data_list = line.split(',')
+            status = data_list[7]
+            if status.lower() == "missing":
+                continue
+            ID = data_list[8]
             first_name = data_list[0]
             last_name = data_list[1]
             name = first_name + " " + last_name
-            ID = data_list[2]
             data_dict[ID] = name
 
     return data_dict
@@ -79,13 +80,13 @@ def _tstrip_lines(lines: str, char = "#"):
         
 
 
-def create_same_code_clusters() -> list[SameCode]:
+def create_same_code_clusters(hw_name) -> list[SameCode]:
     """
     Parses through all the code files and creates a SameCode dataclass for every unique code (after stripping)
     All the SameCode dataclass is returned in a list
     """
     cluster_dict : dict[str, SameCode] = {}
-    data_dict = create_submission_name_map()
+    data_dict = create_submission_name_map(hw_name + '.csv')
     submissions_directory = "./submissions"
     for pathname in os.listdir(submissions_directory):
         if pathname == '.DS_Store' or pathname == 'submission_metadata.yml':
@@ -95,11 +96,13 @@ def create_same_code_clusters() -> list[SameCode]:
         _, fileID = dirname.split('_')
         student_name = str(data_dict.get(fileID))
         for submission_file in os.listdir(os.path.join(submissions_directory, pathname)):
+            if not submission_file.endswith(".py"):
+                continue
             file_name = os.path.basename(submission_file)
             file_path = os.path.join(submissions_directory,pathname,submission_file)
 
             try:
-                with open(file_path) as file:
+                with open(file_path, encoding="utf8", errors='ignore') as file:
                     file_str = file.read().strip()
                     file_str = _tstrip_lines(file_str) # comment this line if top strip not required
                     if file_str not in cluster_dict:
@@ -132,10 +135,11 @@ def filter_same_code_cluster_and_format_lines(same_code_cluster: list[SameCode])
     return lines
 
 def main():
+    hw = "HW6"
     print_init_message()
-    same_code_cluster : list[SameCode] = create_same_code_clusters()
+    same_code_cluster : list[SameCode] = create_same_code_clusters(hw)
     lines : list = filter_same_code_cluster_and_format_lines(same_code_cluster)
-    outfile = "output.csv"
+    outfile = f"{hw}plag.csv"
     with open(outfile, "w") as fobj:
         fobj.write("problem_name,student_names\n")
         fobj.writelines(lines)
@@ -143,4 +147,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-# %%
+ # %%
